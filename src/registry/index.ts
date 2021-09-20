ff714ee9b3495ee1311bc8b3312892fbeeb960d8
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-09-20 07:24:37 +0000

    new config

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,6 +1,13 @@
 import { SDKOptions, ProviderOrSigner } from "../core";
 import { SubSDK } from "../core/sub-sdk";
 import { Registry, Registry__factory } from "../types";
+import { ContractMetadata, getContractMetadata } from "../common/contract";
+
+export interface RegistryControl {
+  address: string;
+  version: number;
+  metadata?: ContractMetadata;
+}
 
 export class RegistrySDK extends SubSDK {
   public readonly contract: Registry;
@@ -16,5 +23,32 @@ export class RegistrySDK extends SubSDK {
       this.address,
       this.providerOrSigner,
     );
+  }
+
+  public async getProtocolContracts(): Promise<RegistryControl[]> {
+    const deployer = await this.getSignerAddress();
+    const maxVersion = this.contract.getLatestVersion(deployer);
+    const versions = Array.from(Array(maxVersion).keys()).reverse();
+    const addresses = await Promise.all(
+      versions.map((i) =>
+        this.contract.getProtocolControl(deployer, i.toString()),
+      ),
+    );
+    const metadatas = await Promise.all(
+      addresses.map((address) =>
+        getContractMetadata(
+          this.providerOrSigner,
+          address,
+          this.opts.ipfsGatewayUrl,
+        ).catch(() => null),
+      ),
+    );
+    return versions.map((v, i) => {
+      return {
+        address: addresses[i],
+        version: v,
+        metadata: metadatas[i],
+      };
+    });
   }
 }
