ab1bdd5b8f7109df689671626752c3baa4e92859
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-10-02 23:09:11 +0000

    fix rinkeby address

--- src/common/registry-address.ts
+++ src/common/registry-address.ts
@@ -5,7 +5,7 @@ export const CONTRACT_ADDRESSES: Record<
   Record<"registry", string>
 > = {
   [ChainId.Rinkeby]: {
-    registry: "0x82576C6E5FC862f6341965EC2216098C199629fB",
+    registry: "0x8e189fbc1babbcd5bb5c960967b34be73867e742",
   },
   [ChainId.Polygon]: {
     registry: "0x8e189fbC1BAbbCd5bb5c960967B34Be73867e742",
