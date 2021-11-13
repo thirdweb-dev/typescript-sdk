539d09e4bb83e2141c443542dd15f3a7e371d9cf
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-11-12 18:27:00 -0800

    Use non-pkey sdk

--- test/app.test.ts
+++ test/app.test.ts
@@ -12,9 +12,10 @@ describe("App Module", async () => {
   let appModule: AppModule;
 
   beforeEach(async () => {
-    sdk = new ThirdwebSDK(
-      new ethers.Wallet(process.env.PKEY, ethers.getDefaultProvider(RPC_URL)),
-    );
+    // sdk = new ThirdwebSDK(
+    //   new ethers.Wallet(process.env.PKEY, ethers.getDefaultProvider(RPC_URL)),
+    // );
+    sdk = new ThirdwebSDK(RPC_URL);
     appModule = sdk.getAppModule("0xA47220197e8c7F7ec462989Ca992b706747B77A8");
   });
 
