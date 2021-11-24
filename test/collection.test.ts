2d529e29b0129501590550f84819795b3192a64b
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-11-23 21:43:07 -0800

    Updated collections test to check bps update call

--- test/collection.test.ts
+++ test/collection.test.ts
@@ -1,6 +1,6 @@
-import { BundleModule, NFTModule, ThirdwebSDK } from "../src/index";
 import * as chai from "chai";
 import { ethers } from "ethers";
+import { BundleModule, NFTModule, ThirdwebSDK } from "../src/index";
 global.fetch = require("node-fetch");
 
 const RPC_URL = "https://matic-mumbai.chainstacklabs.com";
@@ -8,15 +8,12 @@ const RPC_URL = "https://matic-mumbai.chainstacklabs.com";
 describe("Bundle Module (aka Collection Module)", async () => {
   let sdk: ThirdwebSDK;
   let collectionModule: BundleModule;
-  let BundleModule: BundleModule;
+  let bundleModule: BundleModule;
   let nftModule: NFTModule;
 
   beforeEach(async () => {
     sdk = new ThirdwebSDK(
-      new ethers.Wallet(
-        process.env.PKEY,
-        ethers.getDefaultProvider(RPC_URL)
-      )
+      new ethers.Wallet(process.env.PKEY, ethers.getDefaultProvider(RPC_URL)),
     );
     /**
      * This contract address *should* exist forever on mumbai
@@ -24,18 +21,18 @@ describe("Bundle Module (aka Collection Module)", async () => {
      * the test address starting with 0xE79
      */
 
-    BundleModule = sdk.getBundleModule(
-      "0xC70b9AfAfD4f336c6404fc16Ea885418D153ADC1",
-    )
-    /*Testing backwards compatibility*/
-    collectionModule = sdk.getCollectionModule(
-      "0xC70b9AfAfD4f336c6404fc16Ea885418D153ADC1",
+    bundleModule = sdk.getBundleModule(
+      "0x5CF412451f4Cef34293604048238bd18D2BD1e71",
     );
 
-    nftModule = sdk.getNFTModule("0x364A9b8f4382bB583C3833E484A44f7A189312a7")
+    /**
+     * Testing backwards compatibility
+     */
+    collectionModule = sdk.getCollectionModule(
+      "0x5CF412451f4Cef34293604048238bd18D2BD1e71",
+    );
 
-
-
+    nftModule = sdk.getNFTModule("0x364A9b8f4382bB583C3833E484A44f7A189312a7");
   });
 
   it("should return all owned collection tokens", async () => {
@@ -47,22 +44,61 @@ describe("Bundle Module (aka Collection Module)", async () => {
     );
     chai.expect(nfts).to.be.an("array").length.greaterThan(1);
   });
-  it("should create a new collection using token", async () => {
+
+  it.skip("should create a new collection using token", async () => {
     try {
-      await collectionModule.createWithToken("0xbf422E6296770E8750Ff0Ba221EcD7D3f740EE26", 1, {
-        metadata: {},
-        supply: 1,
-      });
+      await collectionModule.createWithToken(
+        "0xbf422E6296770E8750Ff0Ba221EcD7D3f740EE26",
+        1,
+        {
+          metadata: {},
+          supply: 1,
+        },
+      );
     } catch (err) {
       chai.assert.fail(err);
     }
   });
 
-  it("should create a new collection using NFT", async () => {
+  it.skip("should create a new collection using NFT", async () => {
     try {
-      const tokenId = (await nftModule.mint({})).id
-      const owned = await collectionModule.getOwned("0xE79ee09bD47F4F5381dbbACaCff2040f2FbC5803")
-      await collectionModule.createWithERC721("0x364A9b8f4382bB583C3833E484A44f7A189312a7", tokenId, {})
+      const tokenId = (await nftModule.mint({})).id;
+      const owned = await collectionModule.getOwned(
+        "0xE79ee09bD47F4F5381dbbACaCff2040f2FbC5803",
+      );
+      await collectionModule.createWithERC721(
+        "0x364A9b8f4382bB583C3833E484A44f7A189312a7",
+        tokenId,
+        {},
+      );
+    } catch (err) {
+      chai.assert.fail(err);
+    }
+  });
+
+  it("updates the bps in both the metadata and on-chain", async () => {
+    /**
+     * The token with id 1 has been burned and can never be recovered,
+     * so it serves as a good test case.
+     */
+    try {
+      const testBPS = 100;
+      const module = sdk.getBundleModule(
+        "0x54ec360704b2e9E4e6499a732b78094D6d78e37B",
+      );
+      await module.setRoyaltyBps(testBPS);
+      const { metadata } = await module.getMetadata();
+
+      chai.assert.equal(
+        metadata.seller_fee_basis_points,
+        testBPS,
+        "Fetching the BPS from the metadata should return 100",
+      );
+      chai.assert.equal(
+        await module.getRoyaltyBps(),
+        testBPS,
+        "Fetching the BPS with the tx should return 100",
+      );
     } catch (err) {
       chai.assert.fail(err);
     }
