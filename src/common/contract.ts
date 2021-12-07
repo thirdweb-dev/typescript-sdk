837875197a8c8ca168f5491784e2c1d2e869520a
ayush <62694274+ayshptk@users.noreply.github.com>
2021-12-08 01:02:56 +0530

    // test

--- src/common/contract.ts
+++ src/common/contract.ts
@@ -56,13 +56,19 @@ export async function getContractMetadata(
   const uri = await contract.contractURI();
   const gatewayUrl = replaceIpfsWithGateway(uri, ipfsGatewayUrl);
   const meta = await fetch(gatewayUrl);
-  const metadata = await meta.json();
-  const entity: ContractMetadata = {
-    ...metadata,
-    uri,
-    image: replaceIpfsWithGateway(metadata.image, ipfsGatewayUrl),
-  };
-  return entity;
+  try {
+    const metadata = await meta.json();
+    const entity: ContractMetadata = {
+      ...metadata,
+      uri,
+      image: replaceIpfsWithGateway(metadata.image, ipfsGatewayUrl),
+    };
+    return entity;
+  } catch (e) {
+    throw new Error(
+      `Gateway did not return metadata, instead returned:\n ${meta}`,
+    );
+  }
 }
 
 /**
