d94c6caa36d4feb50a35070374a5de2d4b8e0629
ayush <62694274+ayshptk@users.noreply.github.com>
2021-12-08 14:17:25 +0530

    Update batch.test.ts

--- test/batch.test.ts
+++ test/batch.test.ts
@@ -7,7 +7,7 @@ global.fetch = require("node-fetch");
 
 const RPC_URL = "https://matic-mumbai.chainstacklabs.com";
 
-describe("Drop Module", async () => {
+describe("Batch uploading", async () => {
   let sdk: ThirdwebSDK;
   let dropModule: DropModule;
 
