642c4059918d64a0920b52589554b09ac74bcf91
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-12-10 16:19:44 -0800

    Updated text to ensure merkle removal from metadata when no merkle conditions are set

--- test/drop.test.ts
+++ test/drop.test.ts
@@ -64,4 +64,50 @@ describe("Drop Module", async () => {
     );
     expect(roots).length(2);
   });
+
+  it("should remove merkles from the metadata when claim conditions are removed", async () => {
+    const factory = dropModule.getMintConditionsFactory();
+    const phase = factory.newClaimPhase({
+      startTime: new Date(),
+    });
+    await phase.setSnapshot([
+      bobWallet.address,
+      samWallet.address,
+      abbyWallet.address,
+    ]);
+
+    const secondPhase = factory.newClaimPhase({
+      startTime: new Date(),
+    });
+    await secondPhase.setSnapshot([bobWallet.address]);
+
+    console.log("Setting claim condition");
+    await dropModule.setMintConditions(factory);
+    console.log("Claim condition set");
+
+    const { metadata } = await dropModule.getMetadata();
+    const merkles: { [key: string]: string } = metadata["merkle"];
+
+    expect(merkles).have.property(
+      "0x8969b57d06d7a724e0f4891ea35f16ce559df31e5de2ccfed80da1b7f779ef2b",
+    );
+
+    expect(merkles).have.property(
+      "0x575a3a49db748c92b7cc060383de2e0ab0deace6c7c51a90393bebdb680b5be3",
+    );
+
+    const roots = (await dropModule.getAllMintConditions()).map(
+      (c) => c.merkleRoot,
+    );
+    expect(roots).length(2);
+
+    const newFactory = dropModule.getMintConditionsFactory();
+    newFactory.newClaimPhase({
+      startTime: new Date(),
+    });
+    await dropModule.setMintConditions(newFactory);
+    const { metadata: newMetadata } = await dropModule.getMetadata();
+    const newMerkles: { [key: string]: string } = newMetadata["merkle"];
+    expect(JSON.stringify(newMerkles)).to.eq("{}");
+  });
 });
