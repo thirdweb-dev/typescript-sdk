49f2a14fade4bd39b8d1a15de87d3c5bc742b6ed
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-12-13 14:33:25 -0800

    Drop tests working

--- test/drop.test.ts
+++ test/drop.test.ts
@@ -1,22 +1,30 @@
 import { AddressZero } from "@ethersproject/constants";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
+import { expect, assert, use } from "chai";
 import { DropModule } from "../src/index";
-import { appModule, signers } from "./before.test";
+// @ts-ignore
+import { appModule, sdk, signers } from "./before.test";
+import { MerkleTree } from "merkletreejs";
+import * as keccak256 from "keccak256";
 
 global.fetch = require("node-fetch");
 
 describe("Drop Module", async () => {
   let dropModule: DropModule;
-  let adminWallet: SignerWithAddress,
-    samWallet: SignerWithAddress,
-    abbyWallet: SignerWithAddress,
-    bobWallet: SignerWithAddress;
+  let adminWallet,
+    samWallet,
+    abbyWallet,
+    bobWallet,
+    w1,
+    w2,
+    w3,
+    w4: SignerWithAddress;
 
   beforeEach(async () => {
-    [adminWallet, samWallet, bobWallet, abbyWallet] = signers;
+    [adminWallet, samWallet, bobWallet, abbyWallet, w1, w2, w3, w4] = signers;
 
     console.log("Creating drop module");
+    await sdk.setProviderOrSigner(adminWallet);
     dropModule = await appModule.deployDropModule({
       name: "Test Drop",
       maxSupply: 1000,
@@ -46,10 +54,10 @@ describe("Drop Module", async () => {
     console.log("Claim condition set");
 
     const { metadata } = await dropModule.getMetadata();
-    const merkles: { [key: string]: string } = metadata["merkle"];
+    const merkles: { [key: string]: string } = { ...metadata?.merkle };
 
     expect(merkles).have.property(
-      "0x299933cac28b9df1ae6dbf7f5d9814b5fe409a67795ed15dea6135b5fe78c6e3",
+      "0x887a9d7f2b1fca2ff8c07e1e02d906bc2cda73495a8da7494165adcd81875164",
     );
 
     expect(merkles).have.property(
@@ -62,7 +70,7 @@ describe("Drop Module", async () => {
     expect(roots).length(2);
   });
 
-  it.skip("should remove merkles from the metadata when claim conditions are removed", async () => {
+  it("should remove merkles from the metadata when claim conditions are removed", async () => {
     const factory = dropModule.getMintConditionsFactory();
     const phase = factory.newClaimPhase({
       startTime: new Date(),
@@ -83,14 +91,14 @@ describe("Drop Module", async () => {
     console.log("Claim condition set");
 
     const { metadata } = await dropModule.getMetadata();
-    const merkles: { [key: string]: string } = metadata["merkle"];
+    const merkles: { [key: string]: string } = metadata?.merkle;
 
     expect(merkles).have.property(
-      "0x8969b57d06d7a724e0f4891ea35f16ce559df31e5de2ccfed80da1b7f779ef2b",
+      "0x887a9d7f2b1fca2ff8c07e1e02d906bc2cda73495a8da7494165adcd81875164",
     );
 
     expect(merkles).have.property(
-      "0x575a3a49db748c92b7cc060383de2e0ab0deace6c7c51a90393bebdb680b5be3",
+      "0x8a3552d60a98e0ade765adddad0a2e420ca9b1eef5f326ba7ab860bb4ea72c94",
     );
 
     const roots = (await dropModule.getAllMintConditions()).map(
@@ -104,21 +112,25 @@ describe("Drop Module", async () => {
     });
     await dropModule.setMintConditions(newFactory);
     const { metadata: newMetadata } = await dropModule.getMetadata();
-    const newMerkles: { [key: string]: string } = newMetadata["merkle"];
+    const newMerkles: { [key: string]: string } = newMetadata?.merkle;
     expect(JSON.stringify(newMerkles)).to.eq("{}");
   });
 
-  it("test that someone in the merkle tree can successfully claim", async () => {
+  it("allow all addresses in the merkle tree to claim", async () => {
     const factory = dropModule.getMintConditionsFactory();
     const phase = factory.newClaimPhase({
       startTime: new Date(),
     });
-    const members = [
-      adminWallet.address,
-      bobWallet.address,
-      samWallet.address,
-      abbyWallet.address,
+    const testWallets: SignerWithAddress[] = [
+      bobWallet,
+      samWallet,
+      abbyWallet,
+      w1,
+      w2,
+      w3,
+      w4,
     ];
+    const members = testWallets.map((w) => w.address);
     await phase.setSnapshot(members);
 
     console.log("Setting claim condition");
@@ -131,13 +143,18 @@ describe("Drop Module", async () => {
     console.log("Minted");
     console.log("Claiming");
 
-    for (const member of members) {
-      await dropModule.claim(1, []);
-      console.log(`Address ${member} claimed successfully!`);
+    /**
+     * Claiming 1 tokens with proofs: 0xe9707d0e6171f728f7473c24cc0432a9b07eaaf1efed6a137a4a8c12c79552d9,0xb1a5bda84b83f7f014abcf0cf69cab5a4de1c3ececa8123a5e4aaacb01f63f83
+     */
+
+    for (const member of testWallets) {
+      await sdk.setProviderOrSigner(member);
+      await dropModule.claim(1);
+      console.log(`Address ${member.address} claimed successfully!`);
     }
   });
 
-  it.skip("should not allow claiming to someone not in the merkle tree", async () => {
+  it("should not allow claiming to someone not in the merkle tree", async () => {
     const factory = dropModule.getMintConditionsFactory();
     const phase = factory.newClaimPhase({
       startTime: new Date(),
@@ -153,18 +170,61 @@ describe("Drop Module", async () => {
     console.log("Minting");
     await dropModule.lazyMintAmount(1);
     console.log("Minted");
+
+    await sdk.setProviderOrSigner(w1);
     console.log("Claiming");
     try {
       await dropModule.claim(1);
-    } catch {
-      console.log("Could not claim!");
+    } catch (err: any) {
+      expect(err).to.have.property(
+        "message",
+        "No claim found for this address",
+        "",
+      );
       return;
     }
-    throw new Error("Claimed to someone not in the merkle tree");
+    assert.fail("should not reach this point, claim should have failed");
   });
 
-  it.skip("test that a regular claim with no merkle root works as expected", async () => {
+  it("should allow claims with default settings", async () => {
     dropModule.lazyMintAmount(1);
     dropModule.claim(1);
+  });
+
+  it("should generate valid proofs", async () => {
+    const members = [
+      bobWallet.address,
+      samWallet.address,
+      abbyWallet.address,
+      w1.address,
+      w2.address,
+      w3.address,
+      w4.address,
+    ];
+
+    const hashedLeafs = members.map((l) => keccak256(l));
+    const tree = new MerkleTree(hashedLeafs, keccak256, {
+      sort: true,
+      sortLeaves: true,
+      sortPairs: true,
+    });
+    console.log("Root = ", tree.getHexRoot());
+    const snapshot = await sdk.createSnapshot(members);
+    for (const leaf of members) {
+      const expectedProof = tree.getHexProof(keccak256(leaf));
+
+      const actualProof = snapshot.snapshot.claims.find(
+        (c) => c.address === leaf,
+      );
+      assert.isDefined(actualProof);
+      expect(actualProof?.proof).to.include.ordered.members(expectedProof);
+
+      const verified = tree.verify(
+        actualProof?.proof as string[],
+        keccak256(leaf),
+        tree.getHexRoot(),
+      );
+      console.log("Leaf verified =", leaf, verified);
+    }
   });
 });
