0797fe4fad8d89952c3c517b73a68d3a15641596
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2021-12-22 21:32:48 +0000

    balance of supports native token

--- src/common/currency.ts
+++ src/common/currency.ts
@@ -1,8 +1,10 @@
 import { ERC20__factory } from "@3rdweb/contracts";
 import { AddressZero } from "@ethersproject/constants";
+import { Provider } from "@ethersproject/providers";
 import { formatUnits } from "@ethersproject/units";
-import { BigNumberish } from "ethers";
+import { BigNumberish, Signer } from "ethers";
 import { ProviderOrSigner } from "../core/types";
+import { ChainId, SUPPORTED_CHAIN_ID } from "./chain";
 
 /**
  * Currency metadata.
@@ -12,6 +14,14 @@ export interface Currency {
   name: string;
   symbol: string;
   decimals: number;
+}
+
+export interface NativeToken extends Currency {
+  wrapped: {
+    address: string;
+    name: string;
+    symbol: string;
+  };
 }
 
 /**
@@ -29,6 +39,100 @@ export interface CurrencyValue extends Currency {
 export const NATIVE_TOKEN_ADDRESS =
   "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 
+const NATIVE_TOKENS: Record<SUPPORTED_CHAIN_ID | ChainId.Hardhat, NativeToken> =
+  {
+    [ChainId.Mainnet]: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+      wrapped: {
+        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        name: "Wrapped Ether",
+        symbol: "WETH",
+      },
+    },
+    [ChainId.Rinkeby]: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+      wrapped: {
+        address: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+        name: "Wrapped Ether",
+        symbol: "WETH",
+      },
+    },
+    [ChainId.Polygon]: {
+      name: "Matic",
+      symbol: "MATIC",
+      decimals: 18,
+      wrapped: {
+        address: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+        name: "Wrapped Matic",
+        symbol: "WMATIC",
+      },
+    },
+    [ChainId.Mumbai]: {
+      name: "Matic",
+      symbol: "MATIC",
+      decimals: 18,
+      wrapped: {
+        address: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889",
+        name: "Wrapped Matic",
+        symbol: "WMATIC",
+      },
+    },
+    [ChainId.Avalanche]: {
+      name: "Avalanche",
+      symbol: "AVAX",
+      decimals: 18,
+      wrapped: {
+        address: "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+        name: "Wrapped AVAX",
+        symbol: "WAVAX",
+      },
+    },
+    [ChainId.AvalancheFujiTestnet]: {
+      name: "Avalanche",
+      symbol: "AVAX",
+      decimals: 18,
+      wrapped: {
+        address: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c",
+        name: "Wrapped AVAX",
+        symbol: "WAVAX",
+      },
+    },
+    [ChainId.Fantom]: {
+      name: "Fantom",
+      symbol: "FTM",
+      decimals: 18,
+      wrapped: {
+        address: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+        name: "Wrapped Fantom",
+        symbol: "WFTM",
+      },
+    },
+    [ChainId.FantomTestnet]: {
+      name: "Fantom",
+      symbol: "FTM",
+      decimals: 18,
+      wrapped: {
+        address: "0xf1277d1Ed8AD466beddF92ef448A132661956621",
+        name: "Wrapped Fantom",
+        symbol: "WFTM",
+      },
+    },
+    [ChainId.Hardhat]: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+      wrapped: {
+        address: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+        name: "Wrapped Ether",
+        symbol: "WETH",
+      },
+    },
+  };
+
 /**
  * @internal
  */
@@ -38,10 +142,18 @@ export async function getCurrencyMetadata(
 ): Promise<Currency> {
   try {
     if (isNativeToken(asset)) {
+      let provider: Provider;
+      if (Signer.isSigner(providerOrSigner)) {
+        provider = (providerOrSigner as Signer).provider as Provider;
+      } else {
+        provider = providerOrSigner as Provider;
+      }
+      const network = await provider.getNetwork();
+      const nativeToken = getNativeTokenByChainId(network.chainId);
       return {
-        name: "",
-        symbol: "",
-        decimals: 18,
+        name: nativeToken.name,
+        symbol: nativeToken.symbol,
+        decimals: nativeToken.decimals,
       };
     } else {
       const erc20 = ERC20__factory.connect(asset, providerOrSigner);
@@ -89,4 +201,8 @@ export function isNativeToken(tokenAddress: string): boolean {
     tokenAddress.toLowerCase() === NATIVE_TOKEN_ADDRESS ||
     tokenAddress.toLowerCase() === AddressZero
   );
+}
+
+export function getNativeTokenByChainId(chainId: ChainId): NativeToken {
+  return NATIVE_TOKENS[chainId as SUPPORTED_CHAIN_ID];
 }
