903e4e45d9795c7c20320e1fa6c0b25333be5b8e
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-12-22 13:54:40 -0800

    Fixed storage accessor in test

--- test/batch.test.ts
+++ test/batch.test.ts
@@ -34,10 +34,9 @@ describe("Batch uploading", async () => {
     folder.forEach((file) => {
       filelist.push(createReadStream(`test/images/${file}`));
     });
-    const ipfsUri = await dropModule.storage.uploadBatch(
-      filelist,
-      dropModule.address,
-    );
+    const ipfsUri = await sdk
+      .getStorage()
+      .uploadBatch(filelist, dropModule.address);
     const regex = new RegExp(
       /Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,}/,
     );
