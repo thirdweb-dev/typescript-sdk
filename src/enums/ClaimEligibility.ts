faccdf694af85a75a36c59a9f641925387b0ccc2
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2022-01-08 04:53:40 +0000

    can claim on timestamp check

--- src/enums/ClaimEligibility.ts
+++ src/enums/ClaimEligibility.ts
@@ -5,6 +5,8 @@ export enum ClaimEligibility {
 
   WaitBeforeNextClaimTransaction = "Not enough time since last claim transaction. Please wait.",
 
+  AlreadyClaimed = "You have already claimed the token.",
+
   NotEnoughTokens = "There are not enough tokens in the wallet to pay for the claim.",
 
   NoActiveClaimPhase = "There is no active claim phase at the moment. Please check back in later.",
