6177ecd20906a0e8a3e10c279c641082e77d2bfe
Ibrahim Ahmed <abeahmed2@gmail.com>
2022-01-28 09:59:23 -0800

    Exported types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export * from "./core/sdk";
+export * from "./core";
+export * from "./modules";
+export * from "./types";
+export * from "./common";
 export type { ModuleType, NetworkOrSignerOrProvider } from "./core/types";
-export type { DropErc721Module } from "./modules";
