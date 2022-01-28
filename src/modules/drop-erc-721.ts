b0162fedd6765f9b261e19d6e1514ff59d9036a3
Ibrahim Ahmed <abeahmed2@gmail.com>
2022-01-27 17:48:48 -0800

    expose schema

--- src/modules/drop-erc-721.ts
+++ src/modules/drop-erc-721.ts
@@ -61,6 +61,7 @@ export class DropErc721Module {
   // this is a type of readoyly Role[], technically, doing it this way makes it work nicely for types
   // **but** we probably want to enforce in an interface somewhere that `static moduleRoles` is a type of Role[]
   public static moduleRoles = ["admin", "minter", "transfer"] as const;
+  public static schema = DropErc721ModuleSchema;
 
   private storage: IStorage;
 
