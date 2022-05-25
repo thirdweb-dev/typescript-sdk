2d05ea6109b2d750d748c067e96593b33c23c071
adam-maj <mr.adam.maj@gmail.com>
2022-05-25 14:07:30 -0700

    Merge ipfs storage

--- src/core/classes/ipfs-storage.ts
+++ src/core/classes/ipfs-storage.ts
@@ -7,28 +7,8 @@ import {
   replaceHashWithGatewayUrl,
   resolveGatewayUrl,
 } from "../helpers/storage";
-<<<<<<< HEAD
 import { IpfsUploader } from "../storage/ipfs-uploader";
 import { UploadProgressEvent } from "../../types/events";
-=======
-import { isNode } from "../../common/utils";
-
-if (!globalThis.FormData) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  globalThis.FormData = require("form-data");
-}
-
-/**
- * @internal
- */
-interface CidWithFileName {
-  // base cid of the directory
-  cid: string;
-
-  // file name of the file without cid
-  fileNames: string[];
-}
->>>>>>> main
 
 /**
  * IPFS Storage implementation, accepts custom IPFS gateways
@@ -276,82 +256,4 @@ export class IpfsStorage implements IStorage {
     }
     return files;
   }
-<<<<<<< HEAD
-=======
-
-  private async uploadBatchWithCid(
-    files: (string | FileOrBuffer)[],
-    fileStartNumber = 0,
-    contractAddress?: string,
-    signerAddress?: string,
-  ): Promise<CidWithFileName> {
-    const token = await this.getUploadToken(contractAddress || "");
-    const metadata = {
-      name: `CONSOLE-TS-SDK-${contractAddress}`,
-      keyvalues: {
-        sdk: "typescript",
-        contractAddress,
-        signerAddress,
-      },
-    };
-    const data = new FormData();
-    const fileNames: string[] = [];
-    files.forEach((file, i) => {
-      let fileName = "";
-      let fileData = file;
-      // if it is a file, we passthrough the file extensions,
-      // if it is a buffer or string, the filename would be fileStartNumber + index
-      // if it is a buffer or string with names, the filename would be the name
-      if (file instanceof File) {
-        let extensions = "";
-        if (file.name) {
-          const extensionStartIndex = file.name.lastIndexOf(".");
-          if (extensionStartIndex > -1) {
-            extensions = file.name.substring(extensionStartIndex);
-          }
-        }
-        fileName = `${i + fileStartNumber}${extensions}`;
-      } else if (file instanceof Buffer || typeof file === "string") {
-        fileName = `${i + fileStartNumber}`;
-      } else if (file && file.name && file?.data) {
-        fileData = file?.data;
-        fileName = `${file.name}`;
-      } else {
-        // default behavior
-        fileName = `${i + fileStartNumber}`;
-      }
-
-      const filepath = `files/${fileName}`;
-      if (fileNames.indexOf(fileName) > -1) {
-        throw new DuplicateFileNameError(fileName);
-      }
-      fileNames.push(fileName);
-      if (isNode()) {
-        data.append("file", fileData as any, { filepath } as any);
-      } else {
-        // browser does blob things, filepath is parsed differently on browser vs node.
-        // pls pinata?
-        data.append("file", new Blob([fileData as any]), filepath);
-      }
-    });
-
-    data.append("pinataMetadata", JSON.stringify(metadata));
-    const res = await fetch(PINATA_IPFS_URL, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      body: data as any,
-    });
-    const body = await res.json();
-    if (!res.ok) {
-      console.log(body);
-      throw new UploadError("Failed to upload files to IPFS");
-    }
-    return {
-      cid: body.IpfsHash,
-      fileNames,
-    };
-  }
->>>>>>> main
 }
