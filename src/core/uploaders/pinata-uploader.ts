197e3578bffa9b6d6962f58c18e9d320e6b2a44b
adam-maj <mr.adam.maj@gmail.com>
2022-07-18 16:37:59 -0700

    Fix IPFS upload failed error

--- src/core/uploaders/pinata-uploader.ts
+++ src/core/uploaders/pinata-uploader.ts
@@ -70,6 +70,12 @@ export class PinataUploader implements IStorageUpload {
       if (!res.ok) {
         throw new UploadError("Failed to upload files to IPFS");
       }
+
+      const cid = body.IpfsHash;
+      if (!cid) {
+        throw new UploadError("Failed to upload files to IPFS");
+      }
+
       return {
         cid: body.IpfsHash,
         fileNames,
@@ -81,6 +87,15 @@ export class PinataUploader implements IStorageUpload {
         xhr.setRequestHeader("Authorization", `Bearer ${token}`);
 
         xhr.onloadend = () => {
+          if (xhr.status !== 200) {
+            throw new UploadError("Failed to upload files to IPFS");
+          }
+
+          const cid = JSON.parse(xhr.responseText).IpfsHash;
+          if (!cid) {
+            throw new UploadError("Failed to upload files to IPFS");
+          }
+
           resolve({
             cid: JSON.parse(xhr.responseText).IpfsHash,
             fileNames,
